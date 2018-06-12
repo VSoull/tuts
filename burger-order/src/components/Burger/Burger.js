17bcd4a0d0fea7faf06e78ac7ef383cbe9a96b34
LEVI9\v.hetriuc <v.hetriuc@levi9.com>
2018-06-12 16:02:09 +0300

    added buttons for less and more

--- burger-order/src/components/Burger/Burger.js
+++ burger-order/src/components/Burger/Burger.js
@@ -3,11 +3,27 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      console.log('igkey++ ', igKey);
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        // use console.log to see what hapens
+        console.log('igkey+ i', igKey + i);
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+      //[ , ]
+    })
+    .reduce((prevVal, currentValue) => {
+      return prevVal.concat(currentValue);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients.</p>;
+  }
+  console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
