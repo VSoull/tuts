a0276b193d8c20484861a81aa5020963e2fb9ee7
ValentinHC <v8drone12@gmail.com>
2018-06-21 22:42:01 +0300

    added route

---
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './Blog.css';
+import Posts from './Posts/Posts';
+import { Route, Link } from 'react-router-dom';
+import NewPost from '../Blog/NewPost/NewPost';
+
+class Blog extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-sumit=true'
+                  }}
+                >
+                  New Post
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route
+          path="/"
+          exact
+          render={() => <h1>Home</h1>}
+        />
+        <Route path="/" render={() => <h1>Home2</h1>} componet={<Posts />} /> */}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" component={NewPost} />
+      </div>
+    );
+  }
+}
+
+export default Blog;
