81728cd4924ec054f006635d34b26e4567221594
LEVI9\v.hetriuc <v.hetriuc@levi9.com>
2018-07-03 16:34:40 +0300

    start project

---
+++ react-cards/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+// a2dcd2e06f0ef3567b08e37909c67ebf
